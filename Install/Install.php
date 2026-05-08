27e3f7434951872e5e5d94b7ec9ca41ed76d66cc
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2026-05-08 23:43:47 +0530

    Initial Commit

---
+++ Install/Install.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Apps\Tms\Components\Home\Install;
+
+use System\Base\BasePackage;
+
+class Install extends BasePackage
+{
+    public function install()
+    {
+        return true;
+    }
+
+    public function uninstall()
+    {
+        return true;
+    }
+}
---
+++ Widgets.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Apps\Tms\Components\Home;
+
+use System\Base\Providers\ModulesServiceProvider\Modules\Components\ComponentsWidgets;
+
+class Widgets extends ComponentsWidgets
+{}
